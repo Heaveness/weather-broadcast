0505ff828d89f3dbd8cff286f8072d39e33f0c15
Jung In Kim <vindictuslbp@gmail.com>
2023-04-15 17:10:03 -0400

    Added numerous amount of code that adds current forecast and 5-day forecast. Have them display onto HTML as well as fix numerous potential bugs and issues. CSS has been added to reflect the new changes to match mock-up.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-
     var dayDisplayEl = $("#day-display");
-    var submitBtn = $("#submit");
-    var historyEl = $("#history-container");
-    var history = [];
 
     // Variable for modal.
     var modal = document.getElementById("myModal");
@@ -22,56 +18,156 @@ $(document).ready(function () {
     }
     setInterval(displayDay, 1000); // Interval to properly countdown the time.
 
-    function receiveData() {
-        var city = JSON.parse(localStorage.getItem("city"));
+    function receiveData(city) {
         console.log(city);
         var apiKey = "9fd400a4835f3cb9983a527ae1c32bd9";
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&type=like&sort=population&units=metric&appid=" + apiKey;
 
         fetch(queryURL)
             .then((response) => response.json())
-            .then((results) => {
-            const temperature = results.main.temp;
-            const windSpeed = results.wind.speed;
-            const humidity = results.main.humidity;
-            console.log("temperature: " + temperature);
-            console.log("wind speed: " + windSpeed);
-            console.log("humidity: " + humidity);
+            .then((data) => {
+            const temperature = data.main.temp + "°C";
+            const windSpeed = (data.wind.speed * 3.6).toFixed(2) + "km/h";
+            const humidity = data.main.humidity + "%";
+            const weatherIcon = data.weather[0].icon;
+            const iconUrl = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+            
+            document.getElementById("current-1").innerHTML = city;
+            document.getElementById("curTemp").innerHTML = temperature;
+            document.getElementById("curWind").innerHTML = windSpeed;
+            document.getElementById("curHumid").innerHTML = humidity;
+            document.getElementById("curIcon").src = iconUrl;
+            document.getElementById("curIcon").style.visibility = 'visible';
+
+            console.log(weatherIcon)
+            
         })
         .catch((error) => {
             console.error("Error:", error);
         });
     }
 
-    function searchHistory() {
-        var storedCity = JSON.parse(localStorage.getItem("city"));
-        const historyBtn = document.createElement("button");
+    weatherDate();
+    function weatherDate(){
+        var currentDay = dayjs().format("DD/MM/YYYY");
+        $("#current-day").text(currentDay);
 
-        var check = history.includes(storedCity)
-        if (check === true) {
-            return;
-        }
-        else {
-            for (var i = 0; i < history.length; i++) {
-                historyBtn.setAttribute("id", "history");
-                historyBtn.innerText = storedCity;
-                document.body.appendChild(historyBtn);
-                document.getElementById("history-container").append(historyBtn);
-            }
+        const forecastDateData = [];
+
+        for (let i = 1; i < 6; i ++) {
+            var forecastDate = dayjs().add(i, 'day').format("DD/MM/YYYY");
+            forecastDateData.push(forecastDate);
+
+            var forecastDate = document.querySelector(`.future${i}`);
+            forecastDate.innerHTML = forecastDateData[i - 1];
         }
     }
 
-    $("#submit").click(function (event) {
+    function fiveDayForecast(city){
+        var apiKey = "9fd400a4835f3cb9983a527ae1c32bd9";
+        var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&type=like&sort=population&units=metric&appid=" + apiKey;
+
+        const temperatureData = [];
+        const windSpeedData = [];
+        const humidityData = [];
+        const weatherIconData = [];
+
+        fetch(forecastQueryURL)
+            .then((response) => response.json())
+            .then((data) => {
+            const forecastList = data.list;
+
+            for (let i = 1; i < forecastList.length; i += 8) {
+                const forecastData = forecastList[i];
+                const temperature = forecastData.main.temp + "°C";
+                const windSpeed = (forecastData.wind.speed * 3.6).toFixed(2) + "km/h"
+                const humidity = forecastData.main.humidity + "%";
+                const weatherIcon = "https://openweathermap.org/img/w/" + forecastData.weather[0].icon + ".png";
+
+                temperatureData.push(temperature);
+                windSpeedData.push(windSpeed);
+                humidityData.push(humidity);
+                weatherIconData.push(weatherIcon)
+
+                for (let w = 0; w < windSpeedData.length; w++) {
+                    let forecastTemp = document.querySelector(`.futTemp${w + 1}`);
+                    let forecastWind = document.querySelector(`.futWind${w + 1}`);
+                    let forecastHumid = document.querySelector(`.futHumid${w + 1}`);
+                    let forecastIcon = document.querySelector(`.futIcon${w + 1}`);
+
+                    forecastTemp.innerHTML = temperatureData[w];
+                    forecastWind.innerHTML = windSpeedData[w];
+                    forecastHumid.innerHTML = humidityData[w];
+                    forecastIcon.src = weatherIconData[w];
+                    forecastIcon.style.visibility = 'visible';
+                }
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    }
+
+    var storedCity = JSON.parse(localStorage.getItem("city")) || [];
+    searchHistory();
+
+    function searchHistory() {
+        $("#history-container").html("");
+
+        if (storedCity.length === 0) {
+            return;
+        }
+
+        const addedCities = [];
+
+        for (var i = 0; i < storedCity.length; i++) {
+            if (i === 9){
+                break;
+            }
+            if (/^[a-zA-Z\s]+$/.test(storedCity[i])) {
+                if(!addedCities.includes(storedCity[i])){
+                addedCities.push(storedCity[i]);
+                const historyBtn = document.createElement("button");
+                historyBtn.setAttribute("id", "history");
+                historyBtn.innerText = storedCity[i];
+                $("#history-container").append(historyBtn);
+
+                (function (i) {
+                    historyBtn.addEventListener("click", function () {
+                        receiveData(storedCity[i]); // Pass the city at the captured index as a parameter to receiveData()
+                        fiveDayForecast(storedCity[i]);
+                    });
+                })(i);
+            }
+            }
+        }
+    }
+        
+    var searchedHistory = storedCity;
+    $("#submitBtn").click(function (event) {
         event.preventDefault();
 
-        searchedCity = $("#search-text").val().trim().toUpperCase();
-        history.push(searchedCity);
+        var searchedCity = $("#search-text").val().trim().toUpperCase();
+        searchedHistory.push(searchedCity);
 
-        localStorage.setItem("city", JSON.stringify(history));
-        receiveData();
-        searchHistory();
+        if (!isNaN(searchedCity)) {
+            alert("Please enter a valid city name.");
+            return;
+        }
+        else if (searchedCity === "") {
+            alert("Please enter a city name.");
+            return;
+        } 
+        else if (!/^[a-zA-Z]*$/.test(searchedCity)){
+            alert("Please enter a valid city name.");
+            return;
+        } else {
+            localStorage.setItem("city", JSON.stringify(searchedHistory));
+            receiveData(searchedCity);
+            fiveDayForecast(searchedCity);
+            searchHistory();
+        }
     });
-
 
     // Onclick function when the user clicks the button, opens the modal.
     btn.onclick = function () {
